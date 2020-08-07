8430b30ff8f1887160492581f1ffdae7420c1dc3
DenisKalyanov <denzarj@mail.ru>
2020-08-07 18:30:27 +0300

    added validation for User

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,38 @@
 const express = require("express");
 const router = express.Router();
 
+const gravatar = require("gravatar");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/User");
+
 // Drscriptions
-// @route GET api/users
-// @desc Test route
+// @route POST api/users
+// @Register user
 // access Public
 
-router.get("/", (req, res) => {
-  res.send("Users route");
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Include a valid email").isEmail().isLength(),
+    check("password", "Please enter a password witch 6 or more symbols").isLength(6)
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+    } catch (err) {}
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const connectDb = require("./config/db");
 
 const app = express();
 connectDb();
-
+app.use(express.json({ extended: false }));
 //created check page by adress localhost:5000
 app.get("/", (req, res) => {
   res.send("Server is running, display in your browser");
